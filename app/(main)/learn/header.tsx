29ebea6247e207b8a2c5516bcec20d3729dc5ef5
Manoel-junior-mendes-cota <113942719+Manoel-junior-mendes-cota@users.noreply.github.com>
2026-08-21 14:59:11 -0300

    Refactor Header component to use Link inside Button

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -10,15 +10,15 @@ type Props = {
 export const Header = ({ title }: Props) => {
   return (
     <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
-      <Link href="/courses">
-        <Button variant="ghost" size="sm">
+      <Button variant="ghost" size="sm" asChild>
+        <Link href="/courses">
           <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
-        </Button>
-      </Link>
-      <h1 className="font-bold text-lg">
+        </Link>
+      </Button>
+      <h1 className="font-bold text-lg text-neutral-800">
         {title}
       </h1>
-      <div />
+      <div className="w-9 h-9" /> {/* Espaçador com a mesma largura do botão para alinhamento perfeito */}
     </div>
   );
 };
