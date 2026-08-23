ab308830224d6b8bcbae6fa60e6563113b66152d
Manoel-junior-mendes-cota <113942719+Manoel-junior-mendes-cota@users.noreply.github.com>
2026-08-23 09:03:36 -0300

    Refactor Header component for improved layout and text

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
-        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+        <div className="flex items-center gap-x-3">
           <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
             Lingo
@@ -25,18 +25,16 @@ export const Header = () => {
         </ClerkLoading>
         <ClerkLoaded>
           <SignedIn>
-            <UserButton
-              afterSignOutUrl="/"
-            />
+            <UserButton />
           </SignedIn>
           <SignedOut>
             <SignInButton
               mode="modal"
-              afterSignInUrl="/learn"
-              afterSignUpUrl="/learn"
+              fallbackRedirectUrl="/learn"
+              signUpFallbackRedirectUrl="/learn"
             >
               <Button size="lg" variant="ghost">
-                Login
+                Entrar
               </Button>
             </SignInButton>
           </SignedOut>
